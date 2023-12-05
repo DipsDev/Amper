2011868ab64cce37a92a06e41687c867aa9de86c
1Dips <ido.geva.b@gmail.com>
2023-12-05 19:43:55 +0200

    Added actions to perform on a container

--- backend/api/views.py
+++ backend/api/views.py
@@ -26,9 +26,52 @@ class Container(APIView):
         Get specific container info
         '''
         try:
-            container = client.containers.get(kwargs.get("container_id"));
+            container = client.containers.get(kwargs.get("container_id"))
             return Response(container.attrs, status=status.HTTP_200_OK)
         except derrors.NotFound as e:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response({
+                "message": "Couldn't find container"
+            }, status=status.HTTP_404_NOT_FOUND)
             
+    def post(self, request, *args, **kwargs):
+        '''
+
+        Change status of a container     
+        '''
+        # Handle if not supplied action
+        if ("action" not in request.data):
+            return Response({"message": "Missing `action` param"},status=status.HTTP_400_BAD_REQUEST)
+        
+        # Validate the handle is valid
+        action = request.data.get("action")
+        if (action not in ["restart", "start", "stop"]):
+            return Response({"message": "Invalid `action` param"},status=status.HTTP_400_BAD_REQUEST)
+        
+        container_id = kwargs.get("container_id")
+
+        try:
+            container = client.containers.get(container_id)
+        except derrors.NotFound as e:
+            return Response({
+                "message": "Couldn't find container"
+            }, status=status.HTTP_404_NOT_FOUND)
+        
+
+        # Handles actions accordingly
+        try:
+            if (action == "start"):
+                client.containers.run(container_id)
+            elif (action == "stop"):
+                container.stop()
+            else:
+                container.stop()
+                client.containers.run(container_id)
+        except:
+            return Response({ "message": "Error performing action"},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        finally:
+            return Response(status=status.HTTP_200_OK)         
+            
+        
+        
+        
         
